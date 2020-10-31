6db0ee76cc9ebcda6e62c235a5d4999e105d7bd0
JuanSolis <juansolisgtsk@hotmail.com>
2020-10-30 19:56:43 -0600

    Creacion de Libreria de clases con el cifrado de cesar y projecto de consola para probar el cifrado, pruebas con el texto 'MY SPIDER SENSES ARE TINGLING'

---
+++ cifrados/cifrados/Cesar.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace cifrados
+{
+    public class Cesar
+    {
+        public string clave;
+        public List<string> lista = new List<string>();
+        public List<string> listaConClave = new List<string>();
+        public Dictionary<string,string> diccionarioCifrado = new Dictionary<string,string>();
+
+        public string mensajeCifrado;
+        public Cesar() {
+            for (int i = 65; i < 91; i++)
+            {
+                lista.Add(((char)i).ToString());   
+            }
+        }
+
+
+        public void Cifrar(string clave, string mensaje) {
+
+            crearDiccionario(clave);
+
+            string msgCifrado = "";
+            foreach (char caracter in mensaje)
+            {
+                if (diccionarioCifrado.ContainsKey(caracter.ToString()))
+                {
+                    msgCifrado += diccionarioCifrado[caracter.ToString()];
+
+                }
+                else {
+                    msgCifrado += caracter.ToString();
+                }
+            }
+
+            mensajeCifrado = msgCifrado;
+
+        }
+
+        void crearDiccionario(string clave) {
+            agregarClave(clave);
+            agregarRestoDeDiccionario();
+
+            int i = 0;
+            foreach (string item in lista)
+            {
+                diccionarioCifrado.Add(item, listaConClave.ElementAt(i).ToString());
+                i++;
+            }
+        }
+
+        void agregarRestoDeDiccionario() {
+            foreach (string item in lista)
+            {
+                if (!listaConClave.Contains(item))
+                {
+                    listaConClave.Add(item);
+                }
+            }
+
+        }
+        void agregarClave(string clave) {
+
+            foreach (char item in clave)
+            {
+                listaConClave.Add(item.ToString());
+            }
+        }
+
+        public void generarClave() {
+            var chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            var random = new Random();
+            var stringChars = new char[random.Next(chars.Length)];
+
+            char caracter;
+            bool contains = true;
+            for (int i = 0; i < stringChars.Length; i++) {
+
+                while (contains)
+                {
+                    caracter = chars[random.Next(chars.Length)];
+
+                    if (!stringChars.Contains(caracter))
+                    {
+                        stringChars[i] = caracter;
+                        contains = false;
+                    }
+                    else {
+                        contains = true;
+                    }
+                }
+                
+            }
+
+            var finalString = new string(stringChars);
+            clave = finalString;
+        }
+
+    }
+}
---
+++ cifrados/testCifrados/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using cifrados;
+namespace testCifrados
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Cesar cifradoCesar = new Cesar();
+            
+            //cifradoCesar.generarClave();
+
+
+            cifradoCesar.Cifrar("MARYJ", "MY SPIDER SENSES ARE TINGLING");
+            
+            Console.WriteLine(cifradoCesar.mensajeCifrado);
+        }
+    }
+}
